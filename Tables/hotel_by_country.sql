34c21e28c88ed05aa50f60c21670e06be848ddc4
Ezequiel A. Stanganelli <ezequielstanganelli@gmail.com>
2024-07-19 12:49:34 -0300

    Addition of new table

---
+++ Tables/hotel_by_country.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE `hotel_by_country` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID Tip Rate',
+  `country` INT UNSIGNED DEFAULT NULL COMMENT 'ID Country',
+  `rate` VARCHAR(10) DEFAULT NULL COMMENT 'Rate Values',
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb3,
+COLLATE utf8mb3_general_ci,
+COMMENT = 'Tip Rate Hotel by Country';
+
+ALTER TABLE `hotel_by_country` 
+  ADD UNIQUE INDEX country(country);
+
+ALTER TABLE `hotel_by_country` 
+  ADD CONSTRAINT `FK_hotel_by_country_country_id` FOREIGN KEY (country)
+    REFERENCES country(id) ON DELETE SET NULL;
